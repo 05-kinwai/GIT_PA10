562457dc834555e1019dccd11e1bf7724aee619e
05-kinwai <septiceyejack71@gmail.com>
2024-01-23 15:08:12 +0800

    Updated Game

---
+++ Assets/Prefab/ScoreScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+    int score;
+    public Text ScoreText;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Obstacle")
+        {
+            score++;
+            ScoreText.text = " Score : " + score;
+            Destroy(collision.gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
+    int force = 200;
+    public GameObject obstacle;
+    public Text scoretext;
+    public float speed;
+
     private Animation thisAnimation;
 
     void Start()
@@ -15,6 +22,32 @@ public class Player : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
+        {
             thisAnimation.Play();
+            this.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            this.GetComponent<Rigidbody>().AddForce(new Vector3(0,force,0));
+        }
+
+        float VerticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(VerticalInput * speed * Time.deltaTime, 0, 0);
+
+        if (transform.position.y >= 4.15f)
+        {
+            transform.position = new Vector3(3.5f, transform.position.x, transform.position.z);
+        }
+        if (transform.position.y <= -3.84f)
+        {
+
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("GameOver");
+        }
     }
 }
